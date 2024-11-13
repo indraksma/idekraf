13f7808031be019f0f1c7796a58af98b375fe0b0
Indra Kusuma <indra.ksma14@gmail.com>
2024-11-13 13:26:48 +0700

    Ganti Font Login Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,9 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register | {{ config('app.name') }}</title>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Google Font: DM Sans -->
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <!-- icheck bootstrap -->
